18b2ff39b8fbb6779e6936f621e4c1cc60445ac6
Alan7273 <ala.cubillos@duocuc.cl>
2026-09-03 10:15:13 -0400

    Creacion de Data para comandos

---
+++ js/Data.js
@@ -0,0 +1,5 @@
+const juegos = [
+    {id: "g1", nombre: "valorant", minIntegrantes: 5},
+    {id: "g2", nombre: "League of Legends", minIntegrantes: 5},
+    {id: "g3", nombre: "Rocket League", minIntegrantes: 3}
+]
