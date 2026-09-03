f4000efa97c33eea5061db07ac87d0afc67a05c1
Alan7273 <ala.cubillos@duocuc.cl>
2026-09-03 17:04:36 -0400

    Creacion de Data ficticia para validaciones y comandos de javascript

--- js/Data.js
+++
@@ -1,5 +0,0 @@
-const juegos = [
-    {id: "g1", nombre: "valorant", minIntegrantes: 5},
-    {id: "g2", nombre: "League of Legends", minIntegrantes: 5},
-    {id: "g3", nombre: "Rocket League", minIntegrantes: 3}
-]
